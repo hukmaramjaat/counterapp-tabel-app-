2f69f2d2f13fd6fe64588ba10e87e2b0ebcf3f15
hukmaramjaat <hukmaram1999b@gmail.com>
2024-10-06 23:52:10 +0530

    last changes

---
+++ script.js
@@ -0,0 +1,24 @@
+let numberSh = document.querySelector('.container h1')
+const decrement = document.querySelector('#decrement')
+const increment = document.querySelector('#increment')
+const input = document.querySelector('input')
+const rest = document.querySelector('.rest-btn button')
+
+
+decrement.addEventListener('click',() =>{
+   const inputval = parseInt(input.value)
+   const countValue = parseInt(numberSh.innerText)
+    numberSh.innerText = countValue - inputval
+    
+}) 
+
+increment.addEventListener('click',() =>{
+    const inputval = parseInt(input.value)
+   const countValue = parseInt(numberSh.innerText)
+    numberSh.innerText = countValue + inputval;
+}) 
+
+rest.addEventListener('click',() =>{
+    input.value = 0;
+    numberSh.innerText = 0;
+})
